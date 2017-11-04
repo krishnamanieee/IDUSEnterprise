f7b12fa603fcd963eff22deb236494d6728f8b91
Varathalakshmi <varathaathara@gmail.com>
2017-11-04 07:21:04 +0530

    jusy

--- app/src/main/java/com/rohasoft/idus/idus_enterprise/AddCustomerFragment.java
+++ app/src/main/java/com/rohasoft/idus/idus_enterprise/AddCustomerFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -22,6 +26,8 @@ public class AddCustomerFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,7 +70,21 @@ public class AddCustomerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_customer, container, false);
+        View v = inflater.inflate(R.layout.fragment_add_customer, container, false);
+
+
+
+
+
+
+
+
+
+
+        return  v;
+
+
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
