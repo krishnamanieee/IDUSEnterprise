88dc88968bf014c316d8f3de61dba557fa567eb9
kris <krishnamanieee@gmail.com>
2017-11-04 16:32:21 +0530

    add loan , add cus , col loan  .. id setting

---
+++ app/src/main/java/com/rohasoft/idus/idus_enterprise/AddLoan_Activity.java
@@ -0,0 +1,21 @@
+package com.rohasoft.idus.idus_enterprise;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Ayothi selvam on 04-11-2017.
+ */
+
+public class AddLoan_Activity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_laon);
+
+
+    }
+}
